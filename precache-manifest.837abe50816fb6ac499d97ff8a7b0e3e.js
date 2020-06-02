d9d9a205802a1701b8f59cd3e99f1f57e0e40188
JAPGarmando <jarmando_gama@hotmail.com>
2020-06-02 01:43:30 -0500

    Updates

---
+++ precache-manifest.837abe50816fb6ac499d97ff8a7b0e3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da3ce95692ca4e0378389244921327df",
+    "url": "/Fashion-Cats/index.html"
+  },
+  {
+    "revision": "f384be0bfce9c162f3dd",
+    "url": "/Fashion-Cats/static/css/main.8dd4f9ad.chunk.css"
+  },
+  {
+    "revision": "b7025d703d6faac98ebe",
+    "url": "/Fashion-Cats/static/js/2.a0f84a8f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Fashion-Cats/static/js/2.a0f84a8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f384be0bfce9c162f3dd",
+    "url": "/Fashion-Cats/static/js/main.51f0acc2.chunk.js"
+  },
+  {
+    "revision": "52a28ade869c36b7f7ba",
+    "url": "/Fashion-Cats/static/js/runtime-main.2b13473e.js"
+  }
+]);
